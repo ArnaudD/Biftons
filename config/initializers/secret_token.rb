746a718c2ea4318b5b97c8e97874e3c74fd20ae9
Arnaud Didry <arnaud@didry.info>
2011-09-09 11:57:51 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biftons::Application.config.secret_token = 'b36710e7b1f669397753fbfb5bb6f27511fddd494372caf838c81afed7bc918b911a7395587882c62984cc1fb39fb417317351c367667cfb1e9ba07708660ffa'
