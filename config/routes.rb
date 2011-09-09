0cc747f4c2925d08fad87bfa50d8683d8e94537f
Arnaud Didry <arnaud@didry.info>
2011-09-09 15:52:51 +0200

    Install of devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Biftons::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+
+  root :to => 'home#index'
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -46,13 +46,4 @@ Biftons::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
